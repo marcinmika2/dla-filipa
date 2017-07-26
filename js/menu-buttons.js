c0dc2e07d6f042a26572104c37a6f3e21aa4734a
Marcin Mika <marcinmika@hotmail.com>
2017-07-26 20:12:05 +0200

    szkic ciąg dalszy
    sprawdzam jak bedzie zachowywać sie onepage z tyloma danymi do
    wyswietlenia

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -1,4 +1,26 @@
-'use strict';
-    $("button").click(function(){	
-        $("p").slideToggle(1000,'linear',function(){});
-    });
+
+$(document).ready(function(){
+    'use strict';
+
+$('.content').hide()
+	
+		$('.menu-1').click(function(){
+			$('.content').slideUp()
+			$('.menu1').slideDown();
+	});
+	
+		$('.menu-2').click(function(){
+			$('.content').slideUp()
+			$('.menu2').slideDown();
+	});
+	
+		$('.menu-3').click(function(){
+			$('.content').slideUp()
+			$('.menu3').slideDown();
+	});
+	
+		$('.menu-4').click(function(){
+			$('.content').slideUp()
+			$('.menu4').slideDown();
+	});
+});
