a9c206862f7df159f89186ea21a40b16472f2a98
Marcin Mika <marcinmika@hotmail.com>
2017-07-30 14:17:02 +0200

    s

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -5,22 +5,26 @@ $(document).ready(function(){
 $('.content').hide();
 	
 		$('.menu-1').click(function(){
-			$('.content').slideUp()
+			$('.content').slideUp();
 			$('.menu1').slideDown();
+			console.log('dziala');
 	});
 	
 		$('.menu-2').click(function(){
-			$('.content').slideUp()
+			$('.content').slideUp();
 			$('.menu2').slideDown();
+			console.log('dziala');
 	});
 	
 		$('.menu-3').click(function(){
-			$('.content').slideUp()
+			$('.content').slideUp();
 			$('.menu3').slideDown();
+			console.log('dziala');
 	});
 	
 		$('.menu-4').click(function(){
-			$('.content').slideUp()
+			$('.content').slideUp();
 			$('.menu4').slideDown();
+			console.log('dziala');
 	});
 });
