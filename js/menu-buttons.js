0b381b9fdc327c78fdcaa82be0f2718e904531ac
Marcin Mika <marcinmika@hotmail.com>
2017-07-30 14:28:42 +0200

    wszystko działa
    działa: hamburger menu, slideDown, background movie

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -7,24 +7,24 @@ $('.content').hide();
 		$('.menu-1').click(function(){
 			$('.content').slideUp();
 			$('.menu1').slideDown();
-			console.log('dziala');
+	
 	});
 	
 		$('.menu-2').click(function(){
 			$('.content').slideUp();
 			$('.menu2').slideDown();
-			console.log('dziala');
+		
 	});
 	
 		$('.menu-3').click(function(){
 			$('.content').slideUp();
 			$('.menu3').slideDown();
-			console.log('dziala');
+		
 	});
 	
 		$('.menu-4').click(function(){
 			$('.content').slideUp();
 			$('.menu4').slideDown();
-			console.log('dziala');
+			
 	});
 });
